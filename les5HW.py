07db1532ea8e1529401c0731d74b0666f41eda72
Kannika Aleksandr <car113man@gmail.com>
2022-09-22 09:55:57 +0300

    5 home work

--- les5HW.py
+++ les5HW.py
@@ -1,95 +1,125 @@
-# 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# # 1. Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
 
 
-# 2. Создайте программу для игры с конфетами человек против человека.
-#    Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
-#    Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
-#    Все конфеты оппонента достаются сделавшему последний ход. 
-#    Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
-#    a) Добавьте игру против бота
-#    b) Подумайте как наделить бота ""интеллектом""
-import random
+# # 2. Создайте программу для игры с конфетами человек против человека.
+# #    Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# #    Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# #    Все конфеты оппонента достаются сделавшему последний ход. 
+# #    Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# #    a) Добавьте игру против бота
+# #    b) Подумайте как наделить бота ""интеллектом""
+# import random
 
-total = 2021
-was_taken = 0
-player1 = 1
-player2 = 0
-mode = int(input("Выберите режим игры: \n 1 - если с другим игроком \n 2 - если с ботом \n:"))
-if mode == 1:
-    while was_taken < total:
-        move = 0
-        if player1 > player2:
-            move = int(input("1 игрок: "))
-            if move <= 28 and move != 0:
-                was_taken += move
-                player1 -= 1
-                player2 += 1
-            else:
-                print("Число не должно быть меньше 1 и превышать 28!")
-        else:
-            move = int(input("2 игрок: "))
-            if move <= 28 and move != 0:
-                was_taken += move
-                player2 -= 1
-                player1 += 1
-            else:
-                print("Число не должно быть меньше 1 и превышать 28!")
-        print(was_taken)
-elif mode == 2:
-    lavel = int(input("Введите уровень сложности: \n 1 - легкий \n 2 - удачи!!!\n:"))
-    if lavel == 1:
-        while was_taken < total:
-                move = 0
-                if player1 > player2:
-                    move = int(input("Игрок: "))
-                    if move <= 28 and move != 0:
-                        was_taken += move
-                        player1 -= 1
-                        player2 += 1
-                    else:
-                        print("Число не должно быть меньше 1 и превышать 28!")
-                else:
-                    move = random.randint(1,28)
-                    print(f"Компуктер: {move}")
-                    was_taken += move
-                    player2 -= 1
-                    player1 += 1
-                print(was_taken)
-    if lavel == 2:
-        while was_taken < total:
-                move = 0
-                if player1 > player2:
-                    move = int(input("Игрок: "))
-                    if move <= 28 and move != 0:
-                        was_taken += move
-                        player1 -= 1
-                        player2 += 1
-                    else:
-                        print("Число не должно быть меньше 1 и превышать 28!")
-                else:
-                    move = (total - was_taken) % 29 
-                    if move == 0:
-                        move = random.randint(1,28)
-                    print(f"Компуктер: {move}")
-                    was_taken += move
-                    player2 -= 1
-                    player1 += 1
-                print(was_taken)
+# total = 2021
+# was_taken = 0
+# player1 = 1
+# player2 = 0
+# mode = int(input("Выберите режим игры: \n 1 - если с другим игроком \n 2 - если с ботом \n:"))
+# if mode == 1:
+#     while was_taken < total:
+#         move = 0
+#         if player1 > player2:
+#             move = int(input("1 игрок: "))
+#             if move <= 28 and move != 0:
+#                 was_taken += move
+#                 player1 -= 1
+#                 player2 += 1
+#             else:
+#                 print("Число не должно быть меньше 1 и превышать 28!")
+#         else:
+#             move = int(input("2 игрок: "))
+#             if move <= 28 and move != 0:
+#                 was_taken += move
+#                 player2 -= 1
+#                 player1 += 1
+#             else:
+#                 print("Число не должно быть меньше 1 и превышать 28!")
+#         print(was_taken)
+# elif mode == 2:
+#     lavel = int(input("Введите уровень сложности: \n 1 - легкий \n 2 - удачи!!!\n:"))
+#     if lavel == 1:
+#         while was_taken < total:
+#                 move = 0
+#                 if player1 > player2:
+#                     move = int(input("Игрок: "))
+#                     if move <= 28 and move != 0:
+#                         was_taken += move
+#                         player1 -= 1
+#                         player2 += 1
+#                     else:
+#                         print("Число не должно быть меньше 1 и превышать 28!")
+#                 else:
+#                     move = random.randint(1,28)
+#                     print(f"Компуктер: {move}")
+#                     was_taken += move
+#                     player2 -= 1
+#                     player1 += 1
+#                 print(was_taken)
+#     if lavel == 2:
+#         while was_taken < total:
+#                 move = 0
+#                 if player1 > player2:
+#                     move = int(input("Игрок: "))
+#                     if move <= 28 and move != 0:
+#                         was_taken += move
+#                         player1 -= 1
+#                         player2 += 1
+#                     else:
+#                         print("Число не должно быть меньше 1 и превышать 28!")
+#                 else:
+#                     move = (total - was_taken) % 29 
+#                     if move == 0:
+#                         move = random.randint(1,28)
+#                     print(f"Компуктер: {move}")
+#                     was_taken += move
+#                     player2 -= 1
+#                     player1 += 1
+#                 print(was_taken)
 
-    # 3. Создайте программу для игры в ""Крестики-нолики"".
+#     # 3. Создайте программу для игры в ""Крестики-нолики"".
 
-# import re
+import re
 
 
-# a = "| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |"
+board = "| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |"
+b = 1
+cd = 0
+# player_x = input("'x' Ваш ход: ")
+# player_o = input("'o' Ваш ход: ")
+while cd < 9:
+    if b == 1:
+        player_x = input("\n'x' Ваш ход: ")
+        print()
+        board = re.sub(player_x,"x",board)
+        cd += 1
+        b -= 1
+        print(board)
+    else:
+        player_o = input("\n'o' Ваш ход: ")
+        print()
+        board = re.sub(player_o,"o",board)
+        cd += 1
+        b += 1
+        print(board)
 
+# a = re.sub(player_x,"x",a)
+# a = re.sub(player_o,"o",a)
+# print(a)
+
+
+
+
+
+
+b = "4"
+# c = list(map(lambda x: int(x) if x.isdigit() else x, a))
 
 # c ='1'
 # if c == "1":
 #     c = re.sub("1","x",a)
 # print(re.sub(r"\d+", "-", c))
 
-    # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+#     # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
-    # **Входные и выходные данные хранятся в отдельных текстовых файлах.**
+#     # **Входные и выходные данные хранятся в отдельных текстовых файлах.**
