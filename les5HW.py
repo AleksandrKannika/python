1b4aa0439b0e90a09da1e16443106b9b7307a449
Kannika Aleksandr <car113man@gmail.com>
2022-09-22 09:56:29 +0300

    5 home work

--- les5HW.py
+++ les5HW.py
@@ -9,73 +9,73 @@
 # #    Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 # #    a) Добавьте игру против бота
 # #    b) Подумайте как наделить бота ""интеллектом""
-# import random
+import random
 
-# total = 2021
-# was_taken = 0
-# player1 = 1
-# player2 = 0
-# mode = int(input("Выберите режим игры: \n 1 - если с другим игроком \n 2 - если с ботом \n:"))
-# if mode == 1:
-#     while was_taken < total:
-#         move = 0
-#         if player1 > player2:
-#             move = int(input("1 игрок: "))
-#             if move <= 28 and move != 0:
-#                 was_taken += move
-#                 player1 -= 1
-#                 player2 += 1
-#             else:
-#                 print("Число не должно быть меньше 1 и превышать 28!")
-#         else:
-#             move = int(input("2 игрок: "))
-#             if move <= 28 and move != 0:
-#                 was_taken += move
-#                 player2 -= 1
-#                 player1 += 1
-#             else:
-#                 print("Число не должно быть меньше 1 и превышать 28!")
-#         print(was_taken)
-# elif mode == 2:
-#     lavel = int(input("Введите уровень сложности: \n 1 - легкий \n 2 - удачи!!!\n:"))
-#     if lavel == 1:
-#         while was_taken < total:
-#                 move = 0
-#                 if player1 > player2:
-#                     move = int(input("Игрок: "))
-#                     if move <= 28 and move != 0:
-#                         was_taken += move
-#                         player1 -= 1
-#                         player2 += 1
-#                     else:
-#                         print("Число не должно быть меньше 1 и превышать 28!")
-#                 else:
-#                     move = random.randint(1,28)
-#                     print(f"Компуктер: {move}")
-#                     was_taken += move
-#                     player2 -= 1
-#                     player1 += 1
-#                 print(was_taken)
-#     if lavel == 2:
-#         while was_taken < total:
-#                 move = 0
-#                 if player1 > player2:
-#                     move = int(input("Игрок: "))
-#                     if move <= 28 and move != 0:
-#                         was_taken += move
-#                         player1 -= 1
-#                         player2 += 1
-#                     else:
-#                         print("Число не должно быть меньше 1 и превышать 28!")
-#                 else:
-#                     move = (total - was_taken) % 29 
-#                     if move == 0:
-#                         move = random.randint(1,28)
-#                     print(f"Компуктер: {move}")
-#                     was_taken += move
-#                     player2 -= 1
-#                     player1 += 1
-#                 print(was_taken)
+total = 2021
+was_taken = 0
+player1 = 1
+player2 = 0
+mode = int(input("Выберите режим игры: \n 1 - если с другим игроком \n 2 - если с ботом \n:"))
+if mode == 1:
+    while was_taken < total:
+        move = 0
+        if player1 > player2:
+            move = int(input("1 игрок: "))
+            if move <= 28 and move != 0:
+                was_taken += move
+                player1 -= 1
+                player2 += 1
+            else:
+                print("Число не должно быть меньше 1 и превышать 28!")
+        else:
+            move = int(input("2 игрок: "))
+            if move <= 28 and move != 0:
+                was_taken += move
+                player2 -= 1
+                player1 += 1
+            else:
+                print("Число не должно быть меньше 1 и превышать 28!")
+        print(was_taken)
+elif mode == 2:
+    lavel = int(input("Введите уровень сложности: \n 1 - легкий \n 2 - удачи!!!\n:"))
+    if lavel == 1:
+        while was_taken < total:
+                move = 0
+                if player1 > player2:
+                    move = int(input("Игрок: "))
+                    if move <= 28 and move != 0:
+                        was_taken += move
+                        player1 -= 1
+                        player2 += 1
+                    else:
+                        print("Число не должно быть меньше 1 и превышать 28!")
+                else:
+                    move = random.randint(1,28)
+                    print(f"Компуктер: {move}")
+                    was_taken += move
+                    player2 -= 1
+                    player1 += 1
+                print(was_taken)
+    if lavel == 2:
+        while was_taken < total:
+                move = 0
+                if player1 > player2:
+                    move = int(input("Игрок: "))
+                    if move <= 28 and move != 0:
+                        was_taken += move
+                        player1 -= 1
+                        player2 += 1
+                    else:
+                        print("Число не должно быть меньше 1 и превышать 28!")
+                else:
+                    move = (total - was_taken) % 29 
+                    if move == 0:
+                        move = random.randint(1,28)
+                    print(f"Компуктер: {move}")
+                    was_taken += move
+                    player2 -= 1
+                    player1 += 1
+                print(was_taken)
 
 #     # 3. Создайте программу для игры в ""Крестики-нолики"".
 
